e5829d7adc73196c7c39849646d8186374a23bfa
Anddersen <vadim-coder@yandex.ru>
2017-08-16 11:49:52 +0700

    add test state

--- app/containers/Main.jsx
+++ app/containers/Main.jsx
@@ -6,14 +6,24 @@ class Main extends Component
     constructor(props) {
         super(props);
         this.state = {
+            test: false
         }
     }
-
+    showTets(){
+        this.setState({test: !this.state.test});
+    }
     render()
     {
+        const {test} = this.state;
 
+        let testBlock = (<div></div>);
+        if (test) {
+            testBlock = (<div className="test">bla bla</div>);
+        }
         return (
             <div>
+                <div className="show" onClick={()=>this.showTets()}>show test</div>
+                {testBlock}
                 Main
             </div>
         )
