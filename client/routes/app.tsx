7a5c06d47d1587bef8e0048d03586d6724af55b7
Abramov Slava <mail@abramovslava.com>
2017-08-19 16:31:47 +0300

    AN-485

--- client/routes/app.tsx
+++ client/routes/app.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
 
-// import '../static/scss/app.scss';
-
 export class App extends React.Component {
     static propTypes = {
         children: React.PropTypes.node,
     }
+
     render() {
         return (
             <div>
