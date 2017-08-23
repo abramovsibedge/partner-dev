547dfbe8ec68765407b995500be0b7822ccd12e8
Abramov Slava <mail@abramovslava.com>
2017-08-23 08:41:33 +0300

    AN-485

--- client/routes/projects/index.tsx
+++ client/routes/projects/index.tsx
@@ -55,8 +55,8 @@ export class Projects extends React.Component {
 						</div>
 						<div className="header_toolbar">
 							<div className="header_toolbar_content">
-								<div class="project_filter">
-									<button class="btn project_filter_add js-project-create" type="button">
+								<div className="project_filter">
+									<button className="btn project_filter_add js-project-create" type="button">
 										<svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path><path d="M0 0h24v24H0z" fill="none"></path></svg>
 										<span>Add project</span>
 									</button>
