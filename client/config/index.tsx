4041c168951da1c829e628f1c12b321f03eab31c
Abramov Slava <mail@abramovslava.com>
2017-09-01 17:16:38 +0300

    AN-485

--- client/config/index.tsx
+++ client/config/index.tsx
@@ -5,7 +5,7 @@ const storage = new storageHelper;
 const firebaseToken = storage.get('tokens') ? JSON.parse(storage.get('tokens')).firebaseToken : null;
 
 export default {
-	host: 'https://vpn-backend.northghost.com/',
+	host: 'https://backend.northghost.com/',
 	firebaseToken,
 	firebaseKey: ' AIzaSyBjJGXAQW9wuT7O1_bHMoLPaT6YSM1ELGU',
 	firebaseAuthDomain: 'web-portal-for-partners.firebaseapp.com',
