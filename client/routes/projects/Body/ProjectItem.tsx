bc6d8c1e7b0a0da1f94b07230269d79688ef958a
Anddersen <vadim-coder@yandex.ru>
2017-10-02 14:54:40 +0700

    new page of projects

--- client/routes/projects/Body/ProjectItem.tsx
+++ client/routes/projects/Body/ProjectItem.tsx
@@ -7,29 +7,8 @@ import * as classNames from 'classnames';
 import '../../../static/scss/components/modal.scss';
 import '../../../static/scss/components/table.scss';
 
-import {
-	loadProjectItem,
-	deleteProject,
-	addUser,
-	deleteUser,
-	setVisibility
-} from '../../../functions/projects';
-
-import {
-	IconPlus,
-	IconClose,
-	IconDelete
-} from '../../../components/icons'
-
 import Spinner from '../../../components/spinner';
 
-import {
-	Form,
-	FormRow,
-	Input,
-	Checkbox
-} from '../../../components/form';
-import {Button} from '../../../components/button';
 import {emailValidation} from '../../../utils';
 
 
@@ -203,236 +182,38 @@ class ProjectItem extends React.Component<Props, State> {
       project,
     } = this.props;
 
+		console.log(project);
+
 		return (
-			<div className={classNames("table_row", selectedProjectId === project.publickey && "table_row_open")}>
-				<div className="table_row_wrapper" onClick={() => !project.deleteTaskCode && this.openProject(project.publickey)}>
-					<div className="table_cell" style={{width: '30%'}}>
-						<div className="table_cell_content">{project.publickey}</div>
-					</div>
-					<div className="table_cell" style={{width: '50%'}}>
-						<div className="table_cell_content">{project.description}</div>
-					</div>
-					<div className="table_cell" style={{width: '20%'}}>
-						<div className="table_cell_content">{project.deleteTaskCode && <span className="table_label table_label-red">Deleted</span>}</div>
-					</div>
-				</div>
-				<Button type="button" className="project_close" onClick={() => this.closeProject()}>
-					<IconClose width="24" height="24"/>
-				</Button>
+			<div className="item-block">
+				<div className="content">
+					<div className="link-edit">Edit</div>
 
-        <div className={classNames("table_row_content", Object.keys(selectedProject).length === 0 && "is-loading")}>
-
-					{selectedProjectId === project.publickey && Object.keys(selectedProject).length === 0
-          && <Spinner width="65" height="65" strokeWidth="6"/>}
-
-					{selectedProjectId === project.publickey && Object.keys(selectedProject).length > 0
-					&& <div className="project_pane_content">
-						<div className="project_traffic">
-							<table>
-								<thead>
-								<tr>
-									<th>Private Key</th>
-									<th>URL</th>
-								</tr>
-								</thead>
-								<tbody>
-								<tr>
-									<td>{project.privatekey}</td>
-									<td>https://backend.northghost.com</td>
-								</tr>
-								</tbody>
-							</table>
+					<div className="top-info">
+						<div className="img">
+							<img src={require('../../../static/media/def-icon.png')} alt="def" />
 						</div>
-						<div className="project_buttons">
-							<div>
-								<button
-									className={classNames("project_tabs_item", selectedProjectTab === "vpn-servers" && "project_tabs_item-active")}
-									onClick={() => this.tabSwitcher("vpn-servers")} type="button">VPN Servers
-								</button>
-								<button
-									className={classNames("project_tabs_item", selectedProjectTab === "access" && "project_tabs_item-active")}
-									onClick={() => this.tabSwitcher("access")} type="button">Access
-								</button>
-							</div>
-							<div className="project_manage">
-								<Button type="button" className="project_manage_item project_manage_item-disable"
-												onClick={() => this.showDeleteProject(true)}>
-									<IconDelete width="24" height="24"/>
-									<span>Delete project</span>
-								</Button>
-								<Modal
-									isOpen={deleteProjectModalState}
-									className={{base: 'modal_inner'}}
-									overlayClassName={{base: 'modal_outer'}}
-									contentLabel="test">
-									<div className="modal_header">
-										<h2>Delete project</h2>
-									</div>
-									<div className="modal_content is-text-center">Do you really want to delete project?</div>
-									<div className="modal_footer">
-										<button className="modal_btn modal_btn-reset" type="button"
-														onClick={() => this.showDeleteProject(false)}>Cancel
-										</button>
-										<button className="modal_btn modal_btn-submit" type="button"
-														onClick={() => this.deleteProjectConfirm(project)}>Delete project
-										</button>
-									</div>
-									<Button type="button" className="modal_close"
-													onClick={() => this.showDeleteProject(false)}>
-										<IconClose width="24" height="24"/>
-									</Button>
-								</Modal>
+						<p className="name">
+              {project.publickey}
+						</p>
+						<div className="description">
+              {project.description}
+						</div>
+					</div>
+					<div className="block-item-info">
+						<div className="item-info">
+							<p className="name-item-info">Private key</p>
+							<div className="description-item-info">
+                {project.publickey}
 							</div>
 						</div>
-						<div className="project_content">
-							{selectedProjectTab === "vpn-servers" && <div>
-								{selectedProject['countries'].countries.length === 0 &&
-								<div className="project_tabs_empty">
-									<p>Project has no countries.</p>
-								</div>}
-
-								{selectedProject['countries'].countries.length > 0 && <div className="table inner_table">
-									<div className="table_head">
-										<table>
-											<tbody>
-											<tr>
-												<td style={{width: '25%'}}>Country</td>
-												<td style={{width: '65%'}}>Protocols</td>
-												<td style={{width: '10%'}}>Visibility</td>
-											</tr>
-											</tbody>
-										</table>
-									</div>
-									<div className="table_body">
-										{selectedProject['countries'].countries.map((country: any, index: number) => {
-											return <div key={index} className="table_row">
-												<div className="table_row_wrapper">
-													<div className="table_cell" style={{width: '25%'}}>
-														<div className="table_cell_content">{country.country}</div>
-													</div>
-													<div className="table_cell" style={{width: '65%'}}>
-														<div className="table_cell_content">{country.protocols}</div>
-													</div>
-													<div className="table_cell" style={{width: '10%'}}>
-														<div className="table_cell_content country_visibility">
-															<Checkbox
-																className="project_edit_checkbox"
-																checked={country.visibility}
-																onChange={() => this.setVisibility(project.publickey, country.country, !country.visibility)}>&nbsp;</Checkbox>
-														</div>
-													</div>
-												</div>
-											</div>
-										})}
-									</div>
-								</div>}
-							</div>}
-
-							{selectedProjectTab === "access" && <div>
-								<div className="user_new">
-									<Button type="button" className="user_new_btn is-transparent"
-													onClick={() => this.showAddUser(true)}>
-										<IconPlus width="24" height="24"/>
-										<span>Add access email</span>
-									</Button>
-									<Modal
-										isOpen={addUserModalState}
-										className={{base: 'modal_inner'}}
-										overlayClassName={{base: 'modal_outer'}}
-										contentLabel="test">
-										<div className="modal_header">
-											<h2>Create user</h2>
-										</div>
-										<Form submit={() => this.addUserSubmit(project.publickey)} className="modal_form">
-											<div className="modal_error">{addUserObject['message']}</div>
-											<div className="modal_content">
-												<FormRow>
-													<Input
-														type="email"
-														label="User email"
-														value={addUserObject['email']}
-														notValid={!addUserObject['validationState'] && !addUserObject['email']}
-														onChange={(e) => this.addUserHandler(e.target.value, 'email')}>
-													</Input>
-												</FormRow>
-											</div>
-											<div className="modal_footer">
-												<button className="modal_btn modal_btn-reset" type="button"
-																onClick={() => this.showAddUser(false)}>Cancel
-												</button>
-												<button className="modal_btn modal_btn-submit" type="submit">Create user</button>
-											</div>
-										</Form>
-										<Button type="button" className="modal_close" onClick={() => this.showAddUser(false)}>
-											<IconClose width="24" height="24"/>
-										</Button>
-									</Modal>
-								</div>
-								{selectedProject['emails'].usersMail.length === 0 && <div className="project_tabs_empty">
-									<p>Project has no users.</p>
-								</div>}
-
-								{selectedProject['emails'].usersMail.length > 0 && <div className="table inner_table">
-									<div className="table_head">
-										<table>
-											<tbody>
-											<tr>
-												<td style={{width: '50%'}}>User</td>
-												<td style={{width: '50%'}}>&nbsp;</td>
-											</tr>
-											</tbody>
-										</table>
-									</div>
-									<div className="table_body">
-										{selectedProject['emails'].usersMail.map((email: any, index: number) => {
-											return <div key={index} className="table_row">
-												<div className="table_row_wrapper">
-													<div className="table_cell" style={{width: '50%'}}>
-														<div className="table_cell_content">{email}</div>
-													</div>
-													<div className="table_cell" style={{width: '50%'}}>
-														<div className="table_cell_content user_delete">
-															<Button type="button" className="user_delete_btn is-transparent"
-																			onClick={() => this.showDeleteUser(true)}>
-																<IconClose width="24" height="24"/>
-															</Button>
-															<Modal
-																isOpen={deleteUserModalState}
-																className={{base: 'modal_inner'}}
-																overlayClassName={{base: 'modal_outer'}}
-																contentLabel="test">
-																<div className="modal_header">
-																	<h2>Delete user</h2>
-																</div>
-																<div className="modal_content is-text-center">Do you really want to delete
-																	user?
-																</div>
-																<div className="modal_footer">
-																	<button className="modal_btn modal_btn-reset" type="button"
-																					onClick={() => this.showDeleteUser(false)}>Cancel
-																	</button>
-																	<button className="modal_btn modal_btn-submit" type="button"
-																					onClick={() => this.deleteUserConfirm(project.publickey, email)}>
-																		Delete user
-																	</button>
-																</div>
-																<Button type="button" className="modal_close"
-																				onClick={() => this.showDeleteUser(false)}>
-																	<IconClose width="24" height="24"/>
-																</Button>
-															</Modal>
-														</div>
-													</div>
-												</div>
-											</div>
-										})}
-									</div>
-								</div>}
-							</div>}
+						<div className="item-info">
+							<p className="name-item-info">URL</p>
+							<p>https://backend.northghost.com</p>
 						</div>
-					</div>}
-				</div>
+					</div>
 
+				</div>
 			</div>
 		);
 	}
--- client/routes/projects/Body/index.tsx
+++ client/routes/projects/Body/index.tsx
@@ -3,6 +3,7 @@ import * as update from 'immutability-helper';
 import * as classNames from 'classnames';
 
 import ProjectItem from './ProjectItem';
+import '../../../static/scss/components/list-block.scss';
 
 interface Props {
 	onUpdate: () => void
@@ -18,7 +19,6 @@ interface State {
 	selectedProjectId: string
 	selectedProject: object
 	selectedProjectTab: string
-	stickedTableHead: boolean
 }
 
 class Body extends React.Component<Props, State> {
@@ -38,36 +38,10 @@ class Body extends React.Component<Props, State> {
 			selectedProjectId: '',
 			selectedProject: {},
 			selectedProjectTab: 'vpn-servers',
-			stickedTableHead: false
 		}
 	}
 
-	componentDidMount(){
-		window && window.addEventListener('scroll',this.stickTableHead);
-	}
-
-	componentWillUnmount(){
-		window && window.removeEventListener('scroll',this.stickTableHead);
-	}
-
-	stickTableHead = () => {
-		let {stickedTableHead} = this.state;
-
-		window && window.scrollY > 80 ?
-			!stickedTableHead && this.setState(update(this.state, {
-				stickedTableHead: {$set: true}
-			}))
-			:
-			stickedTableHead && this.setState(update(this.state, {
-				stickedTableHead: {$set: false}
-			}));
-	};
-
 	render() {
-		const {
-			stickedTableHead
-		} = this.state;
-
 		const {
 			projects
 		} = this.props;
@@ -80,32 +54,16 @@ class Body extends React.Component<Props, State> {
 						<h1 className="layout_h1">Projects</h1>
 					</div>
 				</header>
-				<div className="layout_content">
-					<div className="table main_table">
-						<div className={classNames("table_head", stickedTableHead && "table_head_sticked")}>
-							<table>
-								<tbody>
-								<tr>
-									<td style={{width: '30%'}}>Public Key</td>
-									<td style={{width: '50%'}}>Description</td>
-									<td style={{width: '20%'}}>&nbsp;</td>
-								</tr>
-								</tbody>
-							</table>
-						</div>
-						<div className="table_body">
-							{projects.length === 0 && <div className="table_row table_row_empty">
-								<div className="table_cell" style={{width: '100%'}}>
-									<div className="table_cell_content">No result for your request.</div>
-								</div>
-							</div>}
 
-							{projects.length > 0 && projects.map((project: any, index: number) => {
-								return <ProjectItem key={index} project={project} onUpdate={this.props.onUpdate} />
-							})}
-						</div>
-					</div>
+				<div className="items-block-content">
+
+					{projects.length === 0 && <div className="center-item-block">No result for your request.</div>}
+
+					{projects.length > 0 && projects.map((project: any, index: number) => {
+						return <ProjectItem key={index} project={project} onUpdate={this.props.onUpdate} />
+					})}
 				</div>
+
 			</section>
 		);
 	}
